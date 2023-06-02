55d410bf53d0414177a4d593a9886c882318d62d
MinhBoss <minhnguyen13032007@gmail.com>
2023-06-02 18:22:14 +0700

    add file13

--- question11.sh
+++
@@ -1,1 +0,0 @@
-git branch branch2 && git checkout branch2 && touch file4 && git add file4 && git commit -m "a" && echo S > file4 && git stash && git checkout main
--- question16.sh
+++
@@ -1,1 +0,0 @@
-git merge branch3 && git branch -d branch3
